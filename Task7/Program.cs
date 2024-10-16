aae12a6b3fb564cc74a7cca68e0b29bdfe3f43b3
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-17 02:58:48 +0300

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Console.WriteLine("Напишите слово: ");
+string word = Console.ReadLine();
+
+bool IsPalindrome(string str)
+{
+    bool plndrm = false;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if(i == str.Length - 1 - i)
+        {
+            plndrm = true;
+        }
+    }
+    return plndrm;
+}
+bool Palindrome =IsPalindrome(word);
+Console.WriteLine(Palindrome ? "палиндром" : "не палиндром");
