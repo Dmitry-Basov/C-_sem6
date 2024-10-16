169b4cfb4c539c849090c5d075133344b595e07e
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-17 01:07:56 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+
+
+using System.Diagnostics.CodeAnalysis;
+
+static string ReverseWord(string strNew)
+{
+    string[] strNewSplit = strNew.Split();
+
+    string res = "";
+    for (int i = strNewSplit.Length - 1; i >= 0; i--)
+    {
+        res += strNewSplit[i] + " ";
+    }
+
+    return res;
+}
+string strNew = "Hello my World";
+string res = ReverseWord(strNew);
+
+Console.WriteLine(res);
