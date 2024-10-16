cc5db4fb1bbb014a6514a86bb926f57c7a2b959c
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-17 01:52:56 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+
+
+string CharArrayToString(char[,] arr)
+{
+    string res = string.Empty;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            res += arr[i, j];
+        }
+    }
+    return res;
+}
+char[,] array = new char[,]{{'a', 'b', 'c'}, {'d', 'e', 'f'}};
+string result = CharArrayToString(array);
+Console.WriteLine(result);
